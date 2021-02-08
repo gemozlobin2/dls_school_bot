43abdf1daace4bbd59973b55da664b080cd3a7cb
Ruslan Miftakhov <ruslan@nzcompare.com>
2021-02-08 17:42:13 +0700

    - Add setting to control deep layer style transfer - Move image manipulation function to handler

--- handlers.py
+++ handlers.py
@@ -1,22 +1,32 @@
 import os
 import numpy as np
+import torchvision
 import torch
 from PIL import Image
-from wkt import WKTModel, img_to_tensor
+from wkt import WKTModel
 from CartoonGAN import Transformer
 
 
 def style_transfer_handler(config, content_img: Image, style_img: Image):
-    model = WKTModel(config["wkt"]["weights_path"], float(config["wkt"]["style_alpha"]), int(config["wkt"]["max_size"]))
-    img_data = model.convert(content_img, style_img)
+    max_size = int(config["wkt"]["max_size"])
+    max_layers = int(config["wkt"]["max_layers"])
+
+    step_size = 2 ** max_layers
+    if max_size < step_size:
+        max_size = step_size
+    elif (max_size % step_size) != 0:
+        max_size = max_size // step_size * step_size
+
+    model = WKTModel(
+        config["wkt"]["weights_path"],
+        float(config["wkt"]["style_alpha"]),
+        max_layers
+    )
+    content = img_to_tensor(content_img, max_size)
+    style = img_to_tensor(style_img, shape=content.shape[1:])
+    img_data = model.convert(content, style)
     del model
-
-    img_data = img_data.to("cpu").clone().detach().numpy().squeeze()
-    img_data = img_data.transpose(1, 2, 0)
-    img_data = img_data * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-    img_data = img_data.clip(0, 1)
-
-    return Image.fromarray((img_data * 255).astype(np.uint8))
+    return tensor_to_img(img_data)
 
 
 def cycle_gan_handler(config, img: Image):
@@ -28,9 +38,36 @@ def cycle_gan_handler(config, img: Image):
     img_data = model(img).squeeze()[[2, 1, 0], :, :]
     del model
 
+    return tensor_to_img(img_data, mean=0.5, std=0.5)
+
+
+def img_to_tensor(img: Image, max_size=256, shape=None, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+    if shape is None:
+        if max(img.size) > max_size:
+            width, height = img.size
+            if width >= height:
+                height = height * max_size // width
+                width = max_size
+            else:
+                width = width * max_size // height
+                height = max_size
+            shape = (height, width)
+        else:
+            shape = (img.size[1], img.size[0])
+
+    transform = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Resize(shape),
+        torchvision.transforms.Normalize(mean, std)
+    ])
+    img = transform(img)
+    img = img[:3, :, :]
+    return img
+
+
+def tensor_to_img(img_data, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
     img_data = img_data.to("cpu").clone().detach().numpy().squeeze()
     img_data = img_data.transpose(1, 2, 0)
-    img_data = img_data * 0.5 + 0.5
+    img_data = img_data * np.array(std) + np.array(mean)
     img_data = img_data.clip(0, 1)
-
     return Image.fromarray((img_data * 255).astype(np.uint8))
--- wkt.py
+++ wkt.py
@@ -1,7 +1,5 @@
 import torch
 import torchfile
-import numpy as np
-import torchvision
 from torch import nn
 from PIL import Image
 
@@ -37,22 +35,19 @@ class ConvWhitening(nn.Module):
 
 
 class WKTModel:
-    def __init__(self, weights_path, style_alpha=0.5, max_size=1000):
+    def __init__(self, weights_path, style_alpha=0.5, max_layers=5):
         self.weights_path = weights_path
         self.device = "cpu"# torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.style_alpha = style_alpha
-        self.max_size = max_size
+        self.max_layers = max_layers
 
-    def convert(self, content_img: Image, style_img: Image):
-        content = img_to_tensor(content_img, self.max_size)
-        style = img_to_tensor(style_img, shape=content.shape[1:])
-
+    def convert(self, content, style):
         # собираем обе картинки в батч для обработки в один проход, сначала контент, потом стилизация
         batch = torch.Tensor([content.tolist(), style.tolist()])
         batch = batch.to(self.device)
 
         with torch.no_grad():
-            for n in range(5, 0, -1):
+            for n in range(self.max_layers, 0, -1):
                 model = nn.Sequential(
                     convert_lua_to_py(f"{self.weights_path}/vgg_normalised_conv{n}_1.t7"),
                     ConvWhitening(self.style_alpha),
@@ -61,11 +56,8 @@ class WKTModel:
                 model.to(self.device)
                 model.eval()
                 content = model(batch)
-
-                # Из-за того что размер изображений не фиксирован, после обработки свертками он может быть
-                # незначительно изменен. Поэтому ресайзим стилевое изображение под новый размер
-                style = img_to_tensor(style_img, shape=content.shape[2:])
-                batch = torch.Tensor([content.squeeze().tolist(), style.tolist()])
+                _, sA, sB, sC = content.shape
+                batch = torch.Tensor([content.squeeze().tolist(), style[:sA, :sB, :sC].tolist()])
                 batch = batch.to(self.device)
 
                 del model
@@ -85,7 +77,7 @@ def get_module_from_lua(obj):
     elif type_name == b'nn.ReLU':
         return nn.ReLU(inplace=True)
     elif type_name == b'nn.SpatialMaxPooling':
-        return nn.MaxPool2d(kernel_size=(o.kH, o.kW), stride=(o.dH, o.dW), padding=(o.padH, o.padW), ceil_mode=o.ceil_mode)
+        return nn.MaxPool2d(kernel_size=(o.kH, o.kW), stride=(o.dH, o.dW), padding=(o.padH, o.padW), ceil_mode=False)
     elif type_name == b'nn.SpatialUpSamplingNearest':
         return nn.UpsamplingNearest2d(scale_factor=o.scale_factor)
 
@@ -96,36 +88,3 @@ def convert_lua_to_py(file_name):
     for i in lua._obj.modules:
         modules.append(get_module_from_lua(i))
     return nn.Sequential(*modules)
-
-
-def img_to_tensor(img: Image, max_size=256, shape=None, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
-    if shape is None:
-        if max(img.size) > max_size:
-            width, height = img.size
-            if width >= height:
-                height = height * max_size // width
-                width = max_size
-            else:
-                width = width * max_size // height
-                height = max_size
-            shape = (height, width)
-        else:
-            shape = (img.size[1], img.size[0])
-
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Resize(shape),
-        torchvision.transforms.Normalize(mean, std)
-    ])
-    img = transform(img)
-    img = img[:3, :, :]
-    return img
-
-
-def tensor_to_img(img_data):
-    img_data = img_data.to("cpu").clone().detach().numpy().squeeze()
-    img_data = img_data.transpose(1, 2, 0)
-    img_data = img_data * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
-    img_data = img_data.clip(0, 1)
-    print(img_data.shape)
-    return Image.fromarray((img_data * 255).astype(np.uint8))
